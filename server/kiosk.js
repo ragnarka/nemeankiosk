2879390edd642bc461b36318c47c49bcf5742011
Ragnar Kalland <ragnar.kallan@gmail.com>
2013-06-09 12:20:06 -0700

    Små endringer

--- server/kiosk.js
+++ server/kiosk.js
@@ -5,6 +5,6 @@
 	Meteor.startup(function() {
 		console.log("Nemean Kiosk is running at server!");
         var test = Meteor.http.get("http://nemean.no/mTest.html");
-        cashiers = new Collection("cashiers");
+        //cashiers = new Collection("cashiers");
 	});
 }(Meteor, _));
