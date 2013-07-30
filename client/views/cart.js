8dbfbbfb79247d08026759b1a84225e79c069d65
Havard Axelsen <havard@wis.no>
2013-07-30 23:06:22 +0200

    Add cart.js

--- client/views/cart.js
+++ client/views/cart.js
@@ -1,5 +1,40 @@
-(function (Meteor) {
+(function (Meteor, $) {
 
     //Insert code here
+    var barcode ="",
+        ENTER = 13;
 
-}(Meteor));
+    function scan() {
+        $("body").keypress(function (e){
+            var keypressed = String.fromCharCode(e.charCode);
+            barcode += keypressed;
+
+            if(e.keyCode === ENTER){
+                alert(barcode);
+                
+                if(findProduct(barcode)){  // === PRODUCT_BARCODE
+                // Add product to basket here!
+                    barcode = "";
+                }
+
+                if(findCashier(barcode))  // === CASHIER_BARCODE
+                // Make sale for Cashier
+                    barcode = "";
+                }
+            }
+        });
+    }
+
+    function findProduct(barcode) {
+        return null;
+    }
+
+    function findCashier(barcode) {
+        return null;
+    }
+
+    Template.cart.rendered = function () {
+        scan();
+    }
+
+}(Meteor, $));
