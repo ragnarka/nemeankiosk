03adc1a973a2b429e1b7b0f12dfac26164b1f0fa
Havard <havardax@gmail.com>
2013-10-07 18:10:55 +0200

    Fix bugs

--- server/kiosk.js
+++ server/kiosk.js
@@ -2,14 +2,14 @@
 
 ( function(Meteor, _) {
 
-    // 
-    Meteor.startup(function() {
-        console.log("Nemean Kiosk is running at server!");
-        Products = new Meteor.Collection("products");
-        console.log("Have fun!");
+        //
+        Meteor.startup(function() {
+            console.log("Nemean Kiosk is running at server!");
+            Products = new Meteor.Collection("products");
+            console.log("Have fun!");
             Orders = new Meteor.Collection("orders");
-        console.log("-------------------------------------------");
-    });
+            console.log("-------------------------------------------");
+        });
 
         // Function imports external products
         function importProducts() {
@@ -26,32 +26,34 @@
             Orders.insert(order);
             console.log(Orders.find({}).fetch());
         }
-    function deleteProduct(barcode) {
-        Products.remove({barcode: barcode});
-    }
 
-    function updateProduct(product) {
-        Products.update({_id: product._id}, {$set: {
-            price: product.price,
-            purchasePrice: product.purchasePrice,
-            amount: product.amount
-        }});
-    }
+        function deleteProduct(barcode) {
+            Products.remove({
+                barcode : barcode
+            });
+        }
 
-    function addProduct(product) {
-        Products.insert(product);
-        console.log("Product " +product.name+ " was added");
-    }
+        function updateProduct(product) {
+            Products.update({
+                _id : product._id
+            }, {
+                $set : {
+                    price : product.price,
+                    purchasePrice : product.purchasePrice,
+                    amount : product.amount
+                }
+            });
+        }
 
-// Publishes product collection
-    Meteor.publish("products", function() {
-        return Products.find({});
-    });
+        function addProduct(product) {
+            Products.insert(product);
+            console.log("Product " + product.name + " was added");
+        }
+
 
         Meteor.methods({
 
             'getProducts' : function() {
-    Meteor.methods({
                 return Products.find().fetch();
             },
 
@@ -61,6 +63,25 @@
                 order.cashier = cashierBarcode;
                 order.date = date;
                 insertOrder(order);
+            },
+
+            // Publishes product collection
+            "addProduct" : function(product) {
+                addProduct(product);
+            },
+
+            "deleteProduct" : function(barcode) {
+                deleteProduct(barcode);
+            },
+
+            "updateProduct" : function(product) {
+                console.log(product);
+                updateProduct(product);
+            },
+
+            "importProducts" : function() {
+                console.log("Hallas");
+                importProducts();
             }
         });
 
@@ -69,25 +90,6 @@
             return Products.find({});
         });
 
-        // Publishes product collection
-        "addProduct": function(product) {
-            addProduct(product);
-        },
-
-        "deleteProduct": function(barcode) {
-            deleteProduct(barcode);
-        },
-
-        "updateProduct": function(product) {
-            console.log(product);
-            updateProduct(product);
-        },
-
-
-        "importProducts": function() {
-            console.log("Hallas");
-            importProducts();
-        }
         Meteor.publish("orders", function() {
             return Orders.find({});
         });
@@ -96,6 +98,5 @@
         Meteor.publish("cashiers", function() {
             return Cashiers.find({});
         });
-    });
 
-}(Meteor, _));
+    }(Meteor, _)); 
